ca68dc4583bbaf91bb9bb7527ce6a854bf453fc0
geo-petrini <geo.petrini@gmail.com>
2025-03-26 18:11:50 +0100

    moved favicon

--- static/js/game.js
+++ static/js/game.js
@@ -11,7 +11,6 @@ class LobbyScene extends Phaser.Scene {
         });
     }
 
-
     preload() {
         // Precarica risorse per la lobby se necessario
         this.load.scenePlugin({
@@ -22,7 +21,6 @@ class LobbyScene extends Phaser.Scene {
             sceneKey: 'rexUI'
         });   
         this.session_id = ''    
-
     }
 
     _getInput(){
@@ -184,15 +182,8 @@ class LobbyScene extends Phaser.Scene {
     }
 }
 
-
-
 class GameScene extends Phaser.Scene {
 
-    // let paddleInfo = document.getElementById('paddle-info');
-    // let sessionInfo = document.getElementById('session-info');
-    // let createSessionButton = document.getElementById('create-session');
-    // let joinSessionButton = document.getElementById('join-session');
-    // let sessionIdInput = document.getElementById('session-id-input');
     constructor(test) {
         super({
             key: 'GameScene'
@@ -207,6 +198,9 @@ class GameScene extends Phaser.Scene {
         this.gameState = {};
         this.paddleSide = 'spectator'; // Predefinito: spettatore
         this.sessionId = data.sessionId;
+
+        this.paddleInfo = document.getElementById('paddle-info');
+        this.sessionInfo = document.getElementById('session-info');
     }
 
     preload () {
@@ -338,169 +332,3 @@ const config = {
 };
 
 const game = new Phaser.Game(config); // eslint-disable-line no-unused-vars
-
-// const config = {
-//     type: Phaser.AUTO,
-//     width: 800,
-//     height: 600,
-//     backgroundColor: '#222',
-//     physics: {
-//         default: 'arcade',
-//         arcade: {
-//             debug: false
-//         }
-//     },
-//     scene: {
-//         preload: preload,
-//         create: create,
-//         update: update
-//     }
-// };
-
-// const game = new Phaser.Game(config);
-
-// let paddleLeft, paddleRight, ball, cursors, keys;
-// let gameState = {};
-// let paddleSide = 'spectator'; // Predefinito: spettatore
-// let sessionId = null; // ID della sessione attuale
-// let paddleInfo = document.getElementById('paddle-info');
-// let sessionInfo = document.getElementById('session-info');
-// let createSessionButton = document.getElementById('create-session');
-// let joinSessionButton = document.getElementById('join-session');
-// let sessionIdInput = document.getElementById('session-id-input');
-
-// function preload() {
-//     const graphics = this.add.graphics();
-//     graphics.fillStyle(0xffffff, 1);
-
-//     const paddleTexture = this.textures.createCanvas('paddle', 10, 100);
-//     graphics.fillRect(0, 0, 10, 100);
-//     graphics.generateTexture('paddle', 10, 100);
-
-//     const ballTexture = this.textures.createCanvas('ball', 10, 10);
-//     graphics.clear();
-//     graphics.fillStyle(0xffffff, 1);
-//     graphics.fillRect(0, 0, 10, 10);
-//     graphics.generateTexture('ball', 10, 10);
-// }
-
-// function create() {
-//     paddleLeft = this.physics.add.image(50, config.height / 2, 'paddle').setImmovable(true);
-//     paddleRight = this.physics.add.image(config.width - 50, config.height / 2, 'paddle').setImmovable(true);
-//     ball = this.physics.add.image(config.width / 2, config.height / 2, 'ball').setCollideWorldBounds(true).setBounce(1);
-
-//     // ball.setVelocity(200, 200);
-
-//     paddleLeft.body.collideWorldBounds = true;
-//     paddleRight.body.collideWorldBounds = true;
-
-//     this.physics.add.collider(ball, paddleLeft);
-//     this.physics.add.collider(ball, paddleRight);
-
-//     cursors = this.input.keyboard.createCursorKeys();
-//     keys = this.input.keyboard.addKeys('W,S');
-
-//     // Connect to the Flask server via Socket.IO
-
-//     socket.on('connect', () => {
-//         console.log('✅ Connesso al server Pong');
-//     });
-
-//     // Riceve l'assegnazione del paddle (sinistra, destra o spettatore)
-//     socket.on('assignPaddle', (data) => {
-//         paddleSide = data.paddle;
-//         updatePaddleInfo();
-//         console.log(`🎮 Paddle assegnato: ${paddleSide}`);
-//     });
-
-//     socket.on('gameState', (state) => {
-//         gameState = state;
-//         updateGameObjects();
-//     });
-// }
-
-// function update() {
-//     if (paddleSide === 'spectator') return; // Spettatori non controllano i paddle
-//     let paddleVelocity = 0;
-
-//     // Controllo del paddle sinistro (W e S)
-//     if (paddleSide === 'left') {
-//         if (keys.W.isDown) paddleVelocity = -300;
-//         else if (keys.S.isDown) paddleVelocity = 300;
-//     }
-
-//     // Controllo del paddle destro (Freccia Su e Giù)
-//     if (paddleSide === 'right') {
-//         if (cursors.up.isDown) paddleVelocity = -300;
-//         else if (cursors.down.isDown) paddleVelocity = 300;
-//     }
-
-//     // Movimento del paddle locale
-//     let currentPaddle = paddleSide === 'left' ? paddleLeft : paddleRight;
-//     currentPaddle.body.setVelocityY(paddleVelocity);
-
-//     // Invia aggiornamento solo se la posizione cambia
-//     if (paddleSide === 'left' && currentPaddle.y !== gameState.paddleLeftY) {
-//         socket.emit('updatePaddle', { paddle: 'left', paddleY: currentPaddle.y });
-//     } else if (paddleSide === 'right' && currentPaddle.y !== gameState.paddleRightY) {
-//         socket.emit('updatePaddle', { paddle: 'right', paddleY: currentPaddle.y });
-//     }
-
-//     // Sincronizza sempre paddle e palla con il server
-//     updateGameObjects();
-// }
-
-// function updateGameObjects() {
-//     if (!gameState) return;
-
-//     // muovi il paddle dell'avversario secondo quanto ricevuto dal server
-//     if (paddleSide === 'left') {
-//         paddleRight.y = gameState.paddleRightY;
-//     }
-//     if (paddleSide === 'right') {
-//         paddleLeft.y = gameState.paddleLeftY;
-//     }
-
-//     // muove la palla secondo quanto ricevuto dal server
-//     ball.x = gameState.ballX;
-//     ball.y = gameState.ballY;
-// }
-
-// function updatePaddleInfo() {
-//     if (paddleSide === 'left') {
-//         paddleInfo.textContent = "🎮 Stai controllando: Sinistra";
-//     } else if (paddleSide === 'right') {
-//         paddleInfo.textContent = "🎮 Stai controllando: Destra";
-//     } else {
-//         paddleInfo.textContent = "👀 Stai osservando la partita";
-//     }
-// }
-    
-
-// // Ascoltiamo l'evento 'sessionCreated' emesso dal server
-// socket.on('sessionCreated', (response) => {
-//     console.debug(`response: ${response}`);
-//     if (response && response.session_id) {
-//         sessionId = response.session_id;
-//         sessionInfo.textContent = `Sessione Creata: ID ${sessionId}`;
-//     } else {
-//         sessionInfo.textContent = 'Errore nella creazione della sessione.';
-//     }
-// });
-
-// // Ascoltiamo l'evento 'sessionJoined' emesso dal server
-// socket.on('sessionJoined', (response) => {
-//     console.debug(`response: ${response}`);
-//     if (response && response.session_id) {
-//         sessionId = response.session_id;
-//         sessionInfo.textContent = `Unito alla sessione: ID ${sessionId}`;
-//     } else {
-//         sessionInfo.textContent = 'Errore nell\'unirsi alla sessione.';
-//     }
-// });
-
-// // Ascoltiamo eventuali errori
-// socket.on('error', (response) => {
-//     console.debug(`error: ${response}`);
-//     sessionInfo.textContent = response.message;  // Mostriamo l'errore nel caso la sessione non sia trovata o sia piena
-// });
